8d97aadb0d085687f95e1cc109d2267bdc1ee2ec
sergiy-chumachenko <chumachenko.sergiy@gmail.com>
2018-07-17 23:18:51 +0300

    [UPDATE] Templates configuration

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,23 @@
+# -*- coding: utf-8 -*-
 from app import app
+from flask import render_template
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }]
+    return render_template('index.html', title='Home', user=user, posts=posts)
