ac72003283e59e5b5a7d90d9f922339b123a27e7
sergiy-chumachenko <chumachenko.sergiy@gmail.com>
2018-08-04 23:33:20 +0300

    [UPDATE] Email Errors and Logs - Part1

--- microblog/config.py
+++ microblog/config.py
@@ -1,10 +1,20 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+try:
+    from secret import ADMINS
+except ImportError:
+    ADMINS = ['your-email@example.com']
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT'), 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ADMINS
